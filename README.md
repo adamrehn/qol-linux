82dfb2b03040920267960f98edd5f4f6d2b7a39b
Adam Rehn <adam@adamrehn.com>
2021-11-28 20:28:27 +1000

    Initial source commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# qol-linux
-Quality of Life (QoL) tools and tweaks for Linux 
+# QoL tools and tweaks for Linux
+
+This repository contains assorted tools and tweaks for Linux that aim to provide small Quality of Life (QoL) improvements to the user experience. Note that these tools and tweaks primarily target distributions that use the **GNOME** desktop environment and may leverage GNOME-specific functionality.
+
+The following tools and tweaks are currently available:
+
+- [**directory-desktop-entry**](./tools/directory-desktop-entry/directory-desktop-entry.py): this is a simple Python script that generates [XDG Desktop Entries](https://specifications.freedesktop.org/desktop-entry-spec/latest/) (`.desktop` files) to open specific directories in the Nautilus file manager, which can then be added to the user's favourites so they appear in the GNOME Shell dash. This provides a roundabout mechanism by which filesystem directories can be added as favourites in the same manner as applications.
---
+++ tools/directory-desktop-entry/directory-desktop-entry.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+from os.path import abspath, basename, exists, expanduser, join
+from pathlib import Path
+from subprocess import run
+import argparse, urllib.parse
+
+
+# The default output directory
+DEFAULT_OUTPUT_DIR='~/.local/share/applications'
+
+# The default icon to use for directories that do not have a custom icon configured
+DEFAULT_ICON = 'org.gnome.Nautilus'
+
+# The template code for generating desktop entries
+DESKTOP_ENTRY_TEMPLATE = '''[Desktop Entry]
+Name={}
+Comment=Open the {} directory
+Keywords=folder;manager;explore;disk;filesystem;
+Exec=nautilus "{}"
+Icon={}
+Terminal=false
+Type=Application
+Categories=GNOME;GTK;Utility;Core;FileManager;
+'''
+
+
+# Parse our command-line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('dir', help='The path to the directory for which a desktop entry should be created')
+parser.add_argument('--outdir', default=DEFAULT_OUTPUT_DIR, help="The output directory to place the generated desktop entry in (defaults to {})".format(DEFAULT_OUTPUT_DIR))
+args = parser.parse_args()
+
+# Verify that the target directory exists
+target = abspath(args.dir)
+if not exists(target):
+	raise RuntimeError('the directory "{}" does not exist!'.format(target))
+
+# Determine if a custom icon has been configured for the target directory
+# (Command details from here: <https://askubuntu.com/a/1235201>)
+icon = DEFAULT_ICON
+key = 'metadata::custom-icon: '
+info = run(['gio', 'info', target], check=True, capture_output=True, universal_newlines=True).stdout.strip()
+lines = [l.strip() for l in info.splitlines()]
+for line in lines:
+	if line.startswith(key):
+		icon = urllib.parse.unquote(line.removeprefix(key)).replace('file://', '')
+
+# Generate the desktop entry
+name = basename(target)
+outfile = join(expanduser(args.outdir), '{}.desktop'.format(name))
+Path(outfile).write_text(DESKTOP_ENTRY_TEMPLATE.format(
+	name,
+	name,
+	target,
+	icon
+))
+
+# Mark the desktop entry as executable
+run(['chmod', '+x', outfile], check=True)
